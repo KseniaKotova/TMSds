1aafe2e25ab46d6ed0eb4d3d8e5048a9afd37bd4
KseniaKotova <ksyu.kotova.79@gmail.com>
2024-11-01 23:59:10 +0300

    Add docstrings

--- diplom/model.py
+++ diplom/model.py
@@ -12,7 +12,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 
 class SymptomClassifier:
-    def __init__(self, csv_path, model_path='symptom_classifier_model.joblib'):
+    """
+    SymptomClassifier is used to classify medical symptoms into specific diagnoses
+    using a trained Random Forest model and a TF-IDF vectorizer for text features.
+
+    Attributes:
+        csv_path (str): Path to the CSV file containing training data.
+        model_path (str): Path to save or load the trained model.
+        stop_words (set): Set of stopwords for text preprocessing.
+        lemmatizer (WordNetLemmatizer): Lemmatizer for word normalization.
+        punct (str): String of punctuation to remove from text.
+        model (RandomForestClassifier): Random Forest model for symptom classification.
+        vectorizer (TfidfVectorizer): TF-IDF vectorizer for text feature extraction.
+    """
+    def __init__(self, csv_path: str, model_path='symptom_classifier_model.joblib'):
         self.csv_path = csv_path
         self.model_path = model_path 
         nltk.download('stopwords')
@@ -27,6 +40,12 @@ class SymptomClassifier:
         self.vectorizer = TfidfVectorizer(max_features=1500)
 
     def load_and_clean_data(self):
+        """
+        Loads and cleans data from the CSV file by removing duplicates, unnecessary labels,
+        and punctuation, and performs tokenization and preprocessing.
+
+        The cleaned data is stored in `self.data`.
+        """
         data = pd.read_csv(self.csv_path)
         data.drop(columns='Unnamed: 0', axis=0, inplace=True)
         data = data[data['label'] != 'Dimorphic Hemorrhoids']
@@ -46,13 +65,36 @@ class SymptomClassifier:
         
         self.data = data
 
-    def preprocess(self, tokens):
+    def preprocess(self, tokens: list) -> list:
+        """
+        Lemmatizes and removes stopwords from tokens.
+
+        Args:
+            tokens (list): List of tokens to preprocess.
+
+        Returns:
+            list: List of processed tokens.
+        """
         return [self.lemmatizer.lemmatize(word.lower()) for word in tokens if word.lower() not in self.stop_words]
 
-    def delete_punct(self, lemmas):
+    def delete_punct(self, lemmas: list) -> list:
+        """
+        Removes punctuation from a list of lemmatized words.
+
+        Args:
+            lemmas (list): List of lemmatized words.
+
+        Returns:
+            list: List of words without punctuation.
+        """
         return [x for x in lemmas if x not in self.punct]
 
     def train_model(self):
+        """
+        Trains the Random Forest model on the cleaned text data, splits the data
+        into training and test sets, evaluates the model on the test set,
+        and saves the model to `self.model_path`.
+        """
         tfidf_features = self.vectorizer.fit_transform(self.data['X']).toarray()
         
         X_train, X_test, y_train, y_test = train_test_split(tfidf_features, self.data['label'], test_size=0.2, random_state=42)
@@ -69,13 +111,27 @@ class SymptomClassifier:
         joblib.dump((self.model, self.vectorizer), self.model_path)
 
     def load_model(self):
+        """
+        Loads a saved model and vectorizer from `self.model_path`. If the model
+        is not found, a message prompts the user to train it first.
+        """
         if os.path.exists(self.model_path):
             self.model, self.vectorizer = joblib.load(self.model_path)
             print("Model loaded successfully.")
         else:
             print("Model file not found. Train the model first by calling `train_model` method.")
 
-    def predict(self, text, top_n=5):
+    def predict(self, text: str, top_n=5) -> list:
+        """
+        Predicts the top diagnoses for a given text input.
+
+        Args:
+            text (str): Symptom description to predict diagnosis for.
+            top_n (int): Number of top predictions to return.
+
+        Returns:
+            list: List of tuples with predicted diagnoses and their probabilities.
+        """
         clean_text = self.preprocess(WordPunctTokenizer().tokenize(text.lower()))
         clean_text = self.delete_punct(clean_text)
         clean_text = ' '.join(clean_text)
@@ -93,13 +149,12 @@ class SymptomClassifier:
 
         return top_results_normalized
 
-# Пример использования:
+## Using:
 # if __name__ == "__main__":
 #     classifier = SymptomClassifier('Symptom2Disease.csv')
 #     classifier.load_and_clean_data()
 #     classifier.train_model()
 
-#     # Пример предсказания с вероятностями для топ-5 диагнозов
 #     symptom_input = "I’ve been experiencing a lot of neck pain lately, also my chest feels tight, my muscles are sore from coughing all the time, and my throat is also quite sore and red. Also my temperature is high."
 #     predictions = classifier.predict(symptom_input, top_n=5)
     
